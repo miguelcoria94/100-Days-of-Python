0b0f39f1baf7d312f6d2092058851aad2789cffb
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-25 04:43:09 -0600

    solve day1.3

--- Day1.py
+++ Day1.py
@@ -1,19 +1,21 @@
 # What am I still doing up!
 
-print("Miguel Coria")
-print("Hello World!")
-print("This is to easy")
+# print("Miguel Coria")
+# print("Hello World!")
+# print("This is to easy")
 
-print("So day 1 is just a bunch of print statements")
+# print("So day 1 is just a bunch of print statements")
 
-print("print('what to print')")
+# print("print('what to print')")
 
-print("this is a test \noh a new line")
+# print("this is a test \noh a new line")
 
-print("helo" + " world")
+# print("helo" + " world")
 
-print("watch your indentations")
+# print("watch your indentations")
 
-input("What is your name? \n")
 
-print("Hello " + input("What is your name? \n"))
+# print("Hello " + input("What is your name? \n"))
+
+print(len(input("what is your name? \a")))
+
