8075b69d761f083dd693f52b028471d32119d146
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-28 13:59:07 -0600

    completed the first challenge

--- Day2.py
+++ Day2.py
@@ -4,6 +4,18 @@
 
 # typeerror when you pass a number to the len function
 
-print("Hello"[0])
+# print("Hello"[0])
 
 # just explaining how we start indexing at 0
+
+# print("123" + "345")
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+print(int(str(two_digit_number)[0]) + int(str(two_digit_number)[1]))
+
+
