92029bf18d392615441532faefd58ba73b936a42
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-28 13:18:02 -0600

    simple print

--- Day2.py
+++ Day2.py
@@ -1,1 +1,9 @@
-GOAL: Make a tip calculator
+# GOAL: Makeatipcalculator
+
+# print(len(33))
+
+# typeerror when you pass a number to the len function
+
+print("Hello"[0])
+
+# just explaining how we start indexing at 0
