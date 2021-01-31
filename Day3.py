0e5a4b3b6e0bf6c33415ffa1c3f06a219ecb4199
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-31 15:40:18 -0600

    starting first challenge

--- Day3.py
+++ Day3.py
@@ -1,7 +1,14 @@
-print("welcome to the rollercoaster!")
-height = int(input("What is your height in cm? \n"))
+# print("welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? \n"))
 
-if height > 120:
-    print("you can ride the rollecoaster!")
-else:
-    print("Sorry, you have to be taller than 120")2
+# if height > 120:
+#     print("you can ride the rollecoaster!")
+# else:
+#     print("Sorry, you have to be taller than 120")2
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
