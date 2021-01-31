3ec4a3aa1d01a0ec8474afa74b45169ae21ebb82
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-31 16:24:07 -0600

    solved leap year coding challenge

--- Day3.py
+++ Day3.py
@@ -20,23 +20,45 @@
 
 # -----------------------------------------------------------
 
-# 🚨 Don't change the code below 👇
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
-# 🚨 Don't change the code above 👆
+# # 🚨 Don't change the code below 👇
+# height = float(input("enter your height in m: "))
+# weight = float(input("enter your weight in kg: "))
+# # 🚨 Don't change the code above 👆
 
-#First *fork* your copy. Then copy-paste your code below this line 👇
-#Finally click "Run" to execute the tests
+# #First *fork* your copy. Then copy-paste your code below this line 👇
+# #Finally click "Run" to execute the tests
 
-bmi = round(weight / (height ** 2))
+# bmi = round(weight / (height ** 2))
 
-if float(bmi) < 18.5:
-    print(f"Your BMI is {bmi}, you are underweight")
-elif bmi <= 25:
-    print(f"Your BMI is {bmi}, you have a normal weight.")
-elif bmi <= 30:
-    print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif bmi <= 35:
-    print(f"Your BMI is {bmi}, you are obese.")
-else:
-    print(f"Your BMI is {bmi}, you are clinically obese.")
+# if float(bmi) < 18.5:
+#     print(f"Your BMI is {bmi}, you are underweight")
+# elif bmi <= 25:
+#     print(f"Your BMI is {bmi}, you have a normal weight.")
+# elif bmi <= 30:
+#     print(f"Your BMI is {bmi}, you are slightly overweight.")
+# elif bmi <= 35:
+#     print(f"Your BMI is {bmi}, you are obese.")
+# else:
+#     print(f"Your BMI is {bmi}, you are clinically obese.")
+
+
+# -----------------------------------------------------------
+
+
+# # 🚨 Don't change the code below 👇
+# year = int(input("Which year do you want to check? "))
+# # 🚨 Don't change the code above 👆
+
+# #First *fork* your copy. Then copy-paste your code below this line 👇
+# #Finally click "Run" to execute the tests
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
