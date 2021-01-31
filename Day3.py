9a7fdeee232c3adc44afc7d69b8caef1d058b0c1
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-31 15:35:19 -0600

    wrote an if statement

---
+++ Day3.py
@@ -0,0 +1,7 @@
+print("welcome to the rollercoaster!")
+height = int(input("What is your height in cm? \n"))
+
+if height > 120:
+    print("you can ride the rollecoaster!")
+else:
+    print("Sorry, you have to be taller than 120")2
